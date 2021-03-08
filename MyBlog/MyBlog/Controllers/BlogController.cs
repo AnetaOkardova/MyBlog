de802c7e342871dae7103d3649a85557394d561f
Aneta O <1.aneta.okardova@gmail.com>
2021-03-08 22:00:27 +0100

    Created MVC template app

---
+++ MyBlog/MyBlog/Controllers/BlogController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyBlog.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyBlog.Controllers
+{
+    public class BlogController : Controller
+    {
+        public IActionResult Index()
+        {
+            var romeImg1 = "C:\\Users\\Aneta\\Desktop\\PhotosMyBlog\\Rome1.jpg";
+            var romeImg2 = "C:\\Users\\Aneta\\Desktop\\PhotosMyBlog\\Rome2.jpg";
+
+            var blog1 = new Blog()
+            {
+                Id = 1,
+                Title = "Exploring Rome",
+                TravelDate = new DateTime(2019, 05, 24),
+                EntryDate = DateTime.Now,
+                ImageURLs = new List<string> { romeImg1, romeImg2},
+                Description = "When in Rome..."
+            };
+
+            return View(blog1);
+        }
+    }
+}
---
+++ MyBlog/MyBlog/Models/Blog.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyBlog.Models
+{
+    public class Blog
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public DateTime EntryDate { get; set; }
+        public DateTime TravelDate { get; set; }
+        public List<string> ImageURLs { get; set; }
+        public string Description { get; set; }
+        
+
+    }
+}
